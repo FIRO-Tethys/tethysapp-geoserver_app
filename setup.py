12ab7f3a539c3eb207c1a8255a15906c13a68bc4
Corey Krewson <ckrewson@aquaveo.com>
2024-02-29 18:36:24 +0000

    tag version v0.0.1_0_2024_02_29

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_namespace_packages
-from tethys_apps.app_installation import find_all_resource_files
-from tethys_apps.base.app_base import TethysAppBase
+from setup_helper import find_all_resource_files
 
 # -- Apps Definition -- #
+namespace = 'tethysapp'
 app_package = 'geoserver_app'
 release_package = f'{TethysAppBase.package_namespace}-{app_package}'
 
